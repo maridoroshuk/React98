581e870d1f5103c20c6ec1c568fdad7feec756eb
maridoroshuk <m.doroshuk@bk.ru>
2023-02-14 12:19:02 +0300

    chore: delete postscss plugin

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,12 +2,6 @@
 const path = require('path');
 
 module.exports = async ({ config }) => {
-  // styles
-  config.module.rules.push({
-    test: /\.(sass|scss)$/,
-    use: ['resolve-url-loader'],
-    include: path.resolve(__dirname, '../')
-  });
   // fonts
   config.module.rules.push({
     test: /\.(png|woff|woff2|eot|ttf|svg)$/,
