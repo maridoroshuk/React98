523629e5e6b2443d49ae2a1e880233618ab64886
maridoroshuk <m.doroshuk@bk.ru>
2023-02-18 11:41:27 +0300

    refactor: webpack build

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,12 @@
 const path = require('path');
 
 module.exports = async ({ config }) => {
-  // fonts
   config.module.rules.push({
     test: /\.(png|woff|woff2|eot|ttf|svg)$/,
     use: [
       {
         loader: 'file-loader',
-        query: {
+        options: {
           name: '[name].[ext]',
         },
       },
